880a332243d53794ef50c9c4a725b54ccdfc86be
Pablo <pablosanchez_964@hotmail.com>
2018-11-10 19:44:00 +0100

    Added clean code features

--- CheckFinalMark.cs
+++ CheckFinalMark.cs
@@ -1,23 +1,38 @@
+/*
+ * Pablo Sánchez Alonso
+ * 
+ * Program that shows the average of three marks.
+ */
+ 
 using System;
 class ChekFinalMark
 {
     static void Main()
     {
+        int mark1, mark2, mark3, average;
+        
         Console.WriteLine("Put your mark of exam one");
-        int mark1 = Convert.ToInt32(Console.ReadLine());
+        mark1 = Convert.ToInt32(Console.ReadLine());
+        
         Console.WriteLine("Put your mark of exam two");
-        int mark2 = Convert.ToInt32(Console.ReadLine());
+        mark2 = Convert.ToInt32(Console.ReadLine());
+        
         Console.WriteLine("Put your mark of exam three");
-        int mark3 = Convert.ToInt32(Console.ReadLine());
-        int average = (mark1 + mark2 + mark3) / 3;
+        mark3 = Convert.ToInt32(Console.ReadLine());
+        
+        average = (mark1 + mark2 + mark3) / 3;
+        
         if (( mark1 >= 4 ) && ( mark1 >= 4 ) && ( mark1 >= 4 ))
         {
             Console.WriteLine("You average is: " + average);
         }
-        else if (( average >= 5 ) && (( mark1 < 4 ) || ( mark2 < 4 ) || ( mark3 < 4 )))
+        
+        else if (( average >= 5 ) && (( mark1 < 4 ) || ( mark2 < 4 ) 
+                || ( mark3 < 4 )))
         {
             Console.WriteLine("You average is: 3");
         }
+        
         else
         {
             Console.WriteLine("You average is: " + average);
